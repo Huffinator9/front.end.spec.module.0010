3d1b8ae19ade9b00f042a3addc9b519987c0232f
Chris 'Huff' Huffer <huffinator@nerdwatchtower.com>
2025-09-01 11:02:35 -0700

    removed project file

--- project/fake.store.app/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-   base: '/front.end.spec.module.0010/',
-   plugins: [react()],
-})
